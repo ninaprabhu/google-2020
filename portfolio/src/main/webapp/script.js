6df19c2c2537fa72d0d28e26a5723bfd359e9338
Nina Prabhu <nsp@stanford.edu>
2020-06-22 20:36:29 +0000

    Move project info to different page

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,17 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+/* Shows picture of dog */
+function showPic(button){
+    document.getElementById('mochi').style.visibility='visible';
+    button.style.visibility="hidden";
 }
