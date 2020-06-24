11bb4570a262e175d6f36789d0e8b04c6d6d1bbf
Nina Prabhu <nsp@stanford.edu>
2020-06-24 18:17:27 +0000

    Add picture page

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,11 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/* Switch to another page */
-function changePage(url){
-    window.location = url;
-}
-
 /* Show picture of dog */
 function showPic(){
     document.getElementById('show').classList.add("hidden");
