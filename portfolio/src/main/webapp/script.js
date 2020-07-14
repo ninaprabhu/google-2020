68129c03f9df0735ce11c98f0e587829b962d0fb
Nina Prabhu <nsp@stanford.edu>
2020-07-14 22:55:46 +0000

    Pass and code meeting query tests

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -103,3 +103,49 @@ function fetchBlobstoreUrl() {
         imageForm.action = imageUploadUrl;
       });
 }
+
+$('.run-functions-button').on('click', function(event) {
+    var $this = $(this);
+    $this.text('...');
+    var $imageSection     = $this.closest('.image-section');
+    var $colorThiefOutput = $imageSection.find('.color-thief-output');
+    var $targetimage      = $imageSection.find('.target-image');
+    showColorsForImage($targetimage, $imageSection);
+  });
+
+  var colorThief = new ColorThief();
+
+  // Run Color Thief functions and display results below image.
+  // We also log execution time of functions for display.
+  var showColorsForImage = function($image, $imageSection ) {
+    var image                    = $image[0];
+    var start                    = Date.now();
+    var color                    = colorThief.getColor(image);
+    var elapsedTimeForGetColor   = Date.now() - start;
+    var palette                  = colorThief.getPalette(image);
+    var elapsedTimeForGetPalette = Date.now() - start + elapsedTimeForGetColor;
+
+    var colorThiefOutput = {
+      color: color,
+      palette: palette,
+      elapsedTimeForGetColor: elapsedTimeForGetColor,
+      elapsedTimeForGetPalette: elapsedTimeForGetPalette
+    };
+    var colorThiefOuputHTML = Mustache.to_html($('#color-thief-output-template').html(), colorThiefOutput);
+
+    $imageSection.addClass('with-color-thief-output');
+    $imageSection.find('.run-functions-button').addClass('hide');
+
+    setTimeout(function(){
+      $imageSection.find('.color-thief-output').append(colorThiefOuputHTML).slideDown();
+
+      // If the color-thief-output div is not in the viewport or cut off, scroll down.
+      var windowHeight          = $(window).height();
+      var currentScrollPosition = $('html').scrollTop()
+      var outputOffsetTop       = $imageSection.find('.color-thief-output').offset().top
+
+      if ((currentScrollPosition < outputOffsetTop) && (currentScrollPosition + windowHeight - 250 < outputOffsetTop)) {
+         $('html, body').animate({scrollTop: outputOffsetTop - windowHeight + 200 + "px"});
+      }
+    }, 300);
+  };
